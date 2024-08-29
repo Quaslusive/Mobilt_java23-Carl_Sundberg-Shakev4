fad6ec75114289ffcd504879bc8aa7ac6853a817
Pawlo Picasso <Quaslusive@protonmail.com>
2024-08-30 00:10:09 +0200

    v0.2

--- app/src/main/java/mobilt_java23/carl_sundberg/shakev4/MainActivity.java
+++ app/src/main/java/mobilt_java23/carl_sundberg/shakev4/MainActivity.java
@@ -1,12 +1,20 @@
 package mobilt_java23.carl_sundberg.shakev4;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,44 +22,140 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private SensorManager mSensorManager;
-    private Sensor mAcceloretor;
-    private SensorEventListener SensorEventListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent sensorEvent) {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor gyroscope;
 
-        }
+    private ProgressBar progressBar;
+    private SeekBar seekBarX, seekBarY, seekBarZ;
+    private ImageView imageView;
+    private TextView textView;
+    private Button button;
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int i) {
-
-        }
-    };
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
 
-    private void SensorAcc() {
+        // Initiera UI-komponenter
+        progressBar = findViewById(R.id.progressBar);
+        seekBarX  = findViewById(R.id.seekBarX);
+        seekBarY  = findViewById(R.id.seekBarY);
+        seekBarZ = findViewById(R.id.seekBarZ);
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
 
+        // Initiera sensorer
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        if (sensorManager == null) {
+            throw new RuntimeException("SensorManager is null. Unable to access SENSOR_SERVICE.");
+        }
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        if (accelerometer == null) {
+            Toast.makeText(this, "No accelerometer found!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (gyroscope == null) {
+            Toast.makeText(this, "No gyroscope found!", Toast.LENGTH_SHORT).show();
+        }
+
+        // Registrera lyssnare om sensorer finns
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if (gyroscope != null) {
+            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
 
+            // Använd accelerometerdata för att uppdatera ProgressBar
+            progressBar.setProgress((int) Math.abs(x * 10));
+
+            seekBarX.setProgress((int)  Math.abs(x * 10));
+            seekBarY.setProgress((int)  Math.abs(y * 10));
+            seekBarZ.setProgress((int)  Math.abs(z * 10));
+
+
+
+            // Kontrollera om en skakning har skett och visa Toast
+            if (Math.abs(x) > 12 || Math.abs(y) > 12 || Math.abs(z) > 12) {
+                Toast.makeText(this, "Shake Detected!", Toast.LENGTH_SHORT).show();
+                Log.i("Shake", "Device shaken!");
+            }
+        }
+
+
+        if (event != null && event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float sides = event.values[0];
+            float upDown = event.values[1];
+
+            imageView.setRotationX(upDown * 3f);
+            imageView.setRotationY(sides * 3f);
+            imageView.setRotation(-sides);
+            imageView.setTranslationX(sides * -10);
+            imageView.setTranslationY(upDown * 10);
+
+
+        }
+    }
+
+
+
+
+
+/*
+
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            float rotationRateX = event.values[0];
+            float rotationRateY = event.values[1];
+            float rotationRateZ = event.values[2];
+
+            // Använd gyroskopdata för att rotera ImageView
+            imageView.setRotation(imageView.getRotation() + rotationRateZ * 10);
+        }
+    }
+*/
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Hantera eventuella ändringar i sensorprecision
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (gyroscope != null) {
+            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 }
