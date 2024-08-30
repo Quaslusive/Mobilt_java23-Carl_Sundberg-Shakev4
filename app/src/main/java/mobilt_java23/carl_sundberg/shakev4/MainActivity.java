a3c2183fcaff731ee7be293f0b47d676e97b1b93
Pawlo Picasso <Quaslusive@protonmail.com>
2024-08-30 17:08:35 +0200

    v1.1

--- app/src/main/java/mobilt_java23/carl_sundberg/shakev4/MainActivity.java
+++ app/src/main/java/mobilt_java23/carl_sundberg/shakev4/MainActivity.java
@@ -1,6 +1,7 @@
 package mobilt_java23.carl_sundberg.shakev4;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.hardware.Sensor;
@@ -27,17 +28,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor accelerometer;
     private Sensor gyroscope;
 
-    private ProgressBar progressBar;
     private SeekBar seekBarX, seekBarY, seekBarZ;
     private ImageView imageView;
-    private TextView textViewX,textViewY, textViewZ, textViewFlatness;
+    private TextView textViewX, textViewY, textViewZ, textViewFlatness;
 
-
+    // Accel data
     float x = 0;
-    float y = 0;
+    float y = 1;
     float z = 2;
+    // Gyro data
+    float zRotation = 2;
 
 
+    @SuppressLint("SuspiciousIndentation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -50,8 +53,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        progressBar = findViewById(R.id.progressBar);
+        // GUI
         seekBarX = findViewById(R.id.seekBarX);
         seekBarY = findViewById(R.id.seekBarY);
         seekBarZ = findViewById(R.id.seekBarZ);
@@ -63,41 +65,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         textViewFlatness = findViewById(R.id.textViewFlatness);
 
 
-
-// TYPE_GYROSCOPE OCH TYPE_ACCELEROMETER
+        // TYPE_GYROSCOPE OCH TYPE_ACCELEROMETER
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-
-
-
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-      //  float tolerance = 3f;
-
 
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             x = event.values[0];
             y = event.values[1];
             z = event.values[2];
 
-            //  Accelerometerdata för att uppdatera ProgressBar och Seekbar
-            progressBar.setProgress((int) Math.abs(x * 10));
-
+            // Accel data för att uppdatera Seekbar
             seekBarX.setProgress((int) Math.abs(x * 10));
             seekBarY.setProgress((int) Math.abs(y * 10));
             seekBarZ.setProgress((int) Math.abs(z * 10));
 
+            // Display Accel data
             textViewX.setText("X: " + x);
             textViewZ.setText("Y: " + y);
             textViewY.setText("Z: " + z);
-
 
 
             // Kontrollera om en skakning har skett och visa Toast
@@ -106,18 +100,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 Log.i("Skakning", "skakning upptäckt");
             }
         }
-
+        // Indikerar flathet via y-axis och x-axis. Grön för maximal flathet, röd för minimal flathet
         int color = ((int) y == 0 && (int) x == 0) ? Color.GREEN : Color.RED;
         textViewFlatness.setBackgroundColor(color);
 
-
+        // "Weird Ball" roterar runt via z-axis
         if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            float zRotation = event.values[2];
+            zRotation = event.values[2];
             imageView.setRotation(imageView.getRotation() + zRotation * 10);
         }
     }
-
-
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -133,12 +125,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume() {
         super.onResume();
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onDestroy() {
+
         super.onDestroy();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
